a0314317cc23e4d2da57b7e38283f3d5d19e4e47
oprogramador <uomodislesia@gmail.com>
2019-02-23 19:13:36 +0100

    fix copy_backup

--- copy_backup.sh
+++ copy_backup.sh
@@ -4,5 +4,5 @@ cd /home/pierre
 mkdir -p backup
 filename=pierre_copy_`date +'%Y-%m-%d_%H-%M-%S'`
 sudo zip --symlinks -r $filename.zip pierre_copy
-zip P__ZIP_E_BACKUP_PASS__ $filename.zip.zip $filename.zip
+zip -P__ZIP_E_BACKUP_PASS__ $filename.zip.zip $filename.zip
 for i in `find pierre_copy*.zip`; do sudo mv $i backup/; done
